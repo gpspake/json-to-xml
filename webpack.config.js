829ea7dd39cf483735a093a0cee7d4af3f8a4418
George Spake <gpspake@gmail.com>
2018-02-17 20:51:55 -0600

    add initial babel config

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,21 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.bundle.js'
   },
-  target: 'node'
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env']
+            ]
+          }
+        }
+      }
+    ]
+  }
 };
