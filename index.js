b3f1433f7f037fda99af6ad0e0e9467e50e02449
George Spake <gpspake@gmail.com>
2018-02-17 22:36:46 -0600

    refactor using xml loader

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-import fs from 'fs';
-import xml2js from 'xml2js';
 import xml from './books.xml';
 
-var parser = new xml2js.Parser();
-fs.readFile(__dirname + '/books.xml', function(err, data) {
-    parser.parseString(data, function (err, result) {
-        console.log(JSON.stringify(result.catalog.book, null, ' '));
-    });
-});
+const xmlToJson = (xml) => JSON.stringify(xml, null, ' ');
 
+console.log('xmltojson', xmlToJson(xml));
