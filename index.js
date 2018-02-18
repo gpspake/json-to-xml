6acafdd8134ae18b57ccb873e5aab5de6b763c87
George Spake <gpspake@gmail.com>
2018-02-17 19:50:23 -0600

    convert xml to json

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+var fs = require('fs'),
+    xml2js = require('xml2js');
+
+var parser = new xml2js.Parser();
+fs.readFile(__dirname + '/books.xml', function(err, data) {
+    parser.parseString(data, function (err, result) {
+        console.log(JSON.stringify(result.catalog.book, null, ' '));
+    });
+});
+
