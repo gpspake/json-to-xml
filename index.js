dc75043d60c0d658aedb4c91eb0e1e076df92868
George Spake <gpspake@gmail.com>
2018-02-17 22:55:14 -0600

    add json to xml

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import xml from './books.xml';
+import jsontoxml from 'jsontoxml'
 
-const xmlToJson = (xml) => JSON.stringify(xml, null, ' ');
+const xmlToJson = JSON.stringify(xml, null, ' ');
+const jsonToXml = jsontoxml(xmlToJson);
 
-console.log('xmltojson', xmlToJson(xml));
+console.log('xmltojson', xmlToJson);
+console.log('jsontoxml', jsonToXml);
